991b13ce72eaae2ff66b276bde3929f25a32b213
Robert Haas <rhaas@postgresql.org>
2015-06-09 15:33:37 -0400

    Add a GUC, delete_needs_where.enabled.
    This can be used to enable or disable the feature on a per-user,
    per-database, or per-session basis (or at any other level at which
    GUCs can be set).

--- contrib/delete_needs_where/delete_needs_where.c
+++ contrib/delete_needs_where/delete_needs_where.c
@@ -14,17 +14,19 @@
 #include "fmgr.h"				/* for PG_MODULE_MAGIC, etc. */
 #include "parser/analyze.h"		/* for parse_analyze_hook_type, etc.e */
 #include "utils/elog.h"			/* for ereport(), etc. */
+#include "utils/guc.h"			/* for DefineCustomBoolVariable */
 
 PG_MODULE_MAGIC;
 
 void		_PG_init(void);
 
 static post_parse_analyze_hook_type original_post_parse_analyze_hook = NULL;
+static bool delete_needs_where_enabled = false;
 
 static void
 delete_needs_where_check(ParseState *pstate, Query *query)
 {
-	if (query->commandType != CMD_DELETE)
+	if (!delete_needs_where_enabled || query->commandType != CMD_DELETE)
 		return;
 
 	Assert(query->jointree != NULL);
@@ -41,6 +43,17 @@ delete_needs_where_check(ParseState *pstate, Query *query)
 void
 _PG_init(void)
 {
+	DefineCustomBoolVariable("delete_needs_where.enabled",
+							 "Require every DELETE statement to have a WHERE clause.",
+							 NULL,
+							 &delete_needs_where_enabled,
+							 false,
+							 PGC_SUSET,
+							 false,
+							 NULL,
+							 NULL,
+							 NULL);
+
 	original_post_parse_analyze_hook = post_parse_analyze_hook;
 	post_parse_analyze_hook = delete_needs_where_check;
 }
